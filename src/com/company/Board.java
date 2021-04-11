f259785b4be7ef588dc214715aa002fe95b5c413
KURDYK louis <kurdyk@informatique.univ-paris-diderot.fr>
2021-04-11 17:05:16 +0200

    Juste board

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -15,45 +15,26 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-   	 private Timer timer;
-   	 private Placeur placeur;
-   	 private boolean ingame;
-   	 private final int ICRAFT_X = 40;
-   	 private final int ICRAFT_Y = 60;
-   	 private int B_WIDTH=this.getWidth();
-   	 private int B_HEIGHT=this.getHeight();
-   	 private final int DELAY = 15;
-   	 public int killRate = 13; //Determiner la mortalité du virus = killRate * 1 / 10000;
-   	 public int infect;
-   	 public int sain;
+	private Timer timer;
+	private Placeur placeur;
+	private boolean ingame;
+	private final int ICRAFT_X = 40;
+	private final int ICRAFT_Y = 60;
+	private int B_WIDTH=this.getWidth();
+	private int B_HEIGHT=this.getHeight();
+	private final int DELAY = 15;
+	public int killRate = 13; //Determiner la mortalité du virus = killRate * 1 / 10000;
+	public int infect;
+	public int sain;
 
 
-	
 	public Board(int i, int j) {
 		initBoard(i,j);
 	}
-	
-	public Board(int i, int j, int h) {
-		initBoard(i,j,h);
-	}
 
-	private void initBoard(int i, int j) {
-		
-		
-		setFocusable(true);
-		setBackground(Color.BLACK);
-		ingame = true;
 
-		setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-		placeur = new Placeur(ICRAFT_X, ICRAFT_Y);
+    private void initBoard(int i, int j) {
 
-		timer = new Timer(DELAY, this);
-        timer.start();
-        placeur.placementIndividus(i, j, false);
-    }
-
-    private void initBoard(int i, int j,int h) {
-    	
         setFocusable(true);
         setBackground(Color.BLACK);
         ingame = true;
@@ -62,11 +43,33 @@ public class Board extends JPanel implements ActionListener {
 
         placeur = new Placeur(ICRAFT_X, ICRAFT_Y);
 
+
         timer = new Timer(DELAY, this);
         timer.start();
-        placeur.placementIndividus(i, j, false,this.B_HEIGHT, this.B_WIDTH);
+        placeur.placementIndividus(i, j, false);
+        System.out.println(i + j);
+		placeur.placeEntreprise(128, 128, "A");
+		placeur.placeEntreprise(256, 256, "B");
+		initLimiteEmploye();
 
 	}
+
+	private void initBoard(int i, int j,int h) {
+
+		setFocusable(true);
+		setBackground(Color.BLACK);
+		ingame = true;
+
+		setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+		placeur = new Placeur(ICRAFT_X, ICRAFT_Y);
+
+		timer = new Timer(DELAY, this);
+		timer.start();
+		placeur.placementIndividus(i, j, false,this.B_HEIGHT, this.B_WIDTH);
+
+	}
+
 
 	@Override
 	public void paintComponent(Graphics g) {
@@ -78,12 +81,18 @@ public class Board extends JPanel implements ActionListener {
 	    }
 
 	private void drawObjects(Graphics g) {
+
 		List<Individu> ls = placeur.getIndividus();
 		for (Individu individu : ls) {
 			if (individu.isVisible()) {
-				g.drawImage(individu.getImage(), individu.getX(),
-						individu.getY(), this);
+				g.drawImage(individu.getImage(), individu.getX(), individu.getY(), this);
 			}
+		}
+		g.setColor(Color.WHITE);
+
+		List<Lieu> lieux = placeur.getLieu();
+		for (Lieu lieu : lieux) {
+			g.drawImage(lieu.getImage(), lieu.getX(), lieu.getY(), this);
 		}
 		g.setColor(Color.WHITE);
 	}
@@ -96,6 +105,8 @@ public class Board extends JPanel implements ActionListener {
 		updateIndividus();
 		CheckCollisionsFutures();
 		refreshAll();
+
+		updateLieux();
 
 		repaint();
 	}
@@ -128,7 +139,10 @@ public class Board extends JPanel implements ActionListener {
 
 
 	private void CheckCollisionsFutures() {
+
 		List<Individu> ls = placeur.getIndividus();
+		List<Lieu> lieux = placeur.getLieu();
+
 		for (Individu individu1 : ls) {
 
 			Rectangle r1 = individu1.getBounds();
@@ -143,19 +157,27 @@ public class Board extends JPanel implements ActionListener {
 					CheckContamination(individu1, individu2);
 				}
 			}
+
+			for (Lieu lieu : lieux) {
+				Rectangle r2 = lieu.getBounds();
+				if (individu1.insideLieu != 1 && r1.intersects(r2)) {
+					lieu.accueil(individu1);
+				}
+			}
 		}
 	}
+
 
     protected void CheckContamination (Individu m, Individu m1) {
         if (m.etat.equals("Infected") && m1.etat.equals("Neutral")) {
             m1.contamine();
-            sain--;
-            infect++;
+			sain--;
+			infect++;
         }
         if (m1.etat.equals("Infected") && m.etat.equals("Neutral")) {
             m.contamine();
-            sain--;
-            infect++;
+			sain--;
+			infect++;
         }
     }
 
@@ -179,8 +201,30 @@ public class Board extends JPanel implements ActionListener {
     		individu.refresh();
     	}
     }
-    
-    int getInfect() {
-    return infect;
-    }
+
+    public void initLimiteEmploye() {
+		List<Employe> ls = placeur.getEmployes();
+		List<Entreprise> lsEntreprise = placeur.getEntreprise();
+
+		if (ls != null && lsEntreprise != null) {
+
+			for (Employe employe : ls) {
+				for (Entreprise entreprise : lsEntreprise) {
+
+					if (employe.entrepriseName.equals(entreprise.nom)) {
+						employe.initEntrepriseDim(entreprise.x, entreprise.y, entreprise.height, entreprise.width);
+					}
+
+				}
+			}
+		}
+	}
+
+	private void updateLieux() {
+		List<Lieu> lieux = placeur.getLieu();
+		for (Lieu lieu : lieux) {
+			lieu.updateAttente();
+		}
+	}
+
 }
