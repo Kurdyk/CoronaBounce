a96c00485b6750a1a1f82fa232d67591b0bdd2fc
Ngoy_John <johnngoy2001@hotmail.fr>
2021-05-03 17:00:15 +0200

    ajout du graph

---
+++ src/com/company/Graph.java
@@ -0,0 +1,99 @@
+package com.company;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class Graph extends JPanel {
+        private int width = 800;
+        private int height = 400;
+        private int padding = 25;
+        private int labelPad = 25;
+        private static final Stroke Graph_Stroke = new BasicStroke(2f);
+        private int pointWidth = 4;
+        private int numberYDiv = 10;
+
+        private Color lineColor = new Color(44, 102, 230, 180);
+        private Color pointColor = new Color(100, 100, 100, 180);
+        private Color gridColor = new Color(200, 200, 200, 200);
+
+
+        private Board board;
+        private int sain;
+        private int infect;
+        private Placeur p;
+        private List<Individu> individu;
+
+        public Graph(Vue view, int sain, int infect) {
+                board = view.board;
+                this.sain = sain;
+                this.infect = infect;
+                p = board.getPlaceur();
+                individu = p.getIndividus();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                double xScale = ((double) getWidth() - (2 * padding) - labelPad) / (individu.size() - 1);
+                double yScale = ((double) getHeight() - (2 * padding) - labelPad) / infect;
+
+
+                /*List<Point> graphPoints = new ArrayList<>();
+                for (int i = 0; i < individu.size(); i++) {
+                        int x1 = (int) (i * xScale + padding + labelPad);
+                        int y1 = (int) ((sain+infect) * yScale + padding);
+                        graphPoints.add(new Point(x1, y1));
+                } a changer par des tics du board*/
+
+
+
+                for (int i = 0; i < numberYDiv + 1; i++) {
+                        int x0 = padding+labelPad;
+                        int x1 = pointWidth+padding+labelPad;
+                        int y0=getHeight() - ((i * (getHeight() - padding * 2 - labelPad)) / numberYDiv + padding + labelPad);
+                        int y1 = y0;
+                        g2.drawLine(x0, y0, x1, y1);
+                }
+                g2.drawLine(padding + labelPad, getHeight() - padding - labelPad, padding + labelPad, padding);
+                g2.drawLine(padding + labelPad, getHeight() - padding - labelPad, getWidth() - padding, getHeight() - padding - labelPad);
+
+                Stroke oldStroke = g2.getStroke();
+                g2.setColor(lineColor);
+                g2.setStroke(Graph_Stroke);
+                /*for (int i = 0; i < graphPoints.size() - 1; i++) {
+                        int x1 = graphPoints.get(i).x;
+                        int y1 = graphPoints.get(i).y;
+                        int x2 = graphPoints.get(i + 1).x;
+                        int y2 = graphPoints.get(i + 1).y;
+                        g2.drawLine(x1, y1, x2, y2);
+                }
+
+                g2.setStroke(oldStroke);
+                g2.setColor(pointColor);
+                for (int i = 0; i < graphPoints.size(); i++) {
+                        int x = graphPoints.get(i).x - pointWidth / 2;
+                        int y = graphPoints.get(i).y - pointWidth / 2;
+                        int ovalW = pointWidth;
+                        int ovalH = pointWidth;
+                        g2.fillOval(x, y, ovalW, ovalH);
+                }*/
+
+        }
+
+}
--- src/com/company/Vue.java
+++ src/com/company/Vue.java
@@ -8,10 +8,11 @@ import java.awt.*;
 public class Vue extends JFrame{ //classe qui créer la fenêtre contenant la simulation
 	Board board ;
 
-	
+
 	Vue(boolean[] act, int[] val){ //constructeur qui va créer la fenêtre
 		this.setTitle("Simulation"); //fixe le titre de la fenêtre
 		this.setSize(700, 800); //fixe les dimensions de la fenêtre
+		this.setLocation(0, Toolkit.getDefaultToolkit().getScreenSize().height - 400 );
 
 		this.board=new Board(act, val); //créer la board avec les paramètre que l'utilisateurs a donné
 
